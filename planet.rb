4628a794c9a20c908d2b71f17ac527f419541be8
Sandy <sandyvasquez9@gmail.com>
2020-09-24 16:46:59 -0700

    update planet.rb

--- planet.rb
+++ planet.rb
@@ -1,17 +1,14 @@
-class Planet
-  attr_reader :name, :color, :mass_kg, :distance_from_sun_km, :fun_fact
-  def initialize(name, color, mass_kg, distance_from_sun_km, fun_fact)
+class Alien_Type
+  attr_reader :name, :color, :home, :distance_from_sun_ly, :fun_fact
+  def initialize(name, color, distance_from_sun_ly, home, fun_fact)
     @name = name
     @color = color
-    @mass_kg = mass_kg
-    @distance_from_sun_km =  distance_from_sun_km
+    @distance_from_sun_ly =  distance_from_sun_ly
+    @home = home
     @fun_fact = fun_fact
   end
-
   def summary
-    return "#{@name}, is the color #{@color} and has a mass of #{@mass_kg} kilograms. In addition, it is
-    #{@distance_from_sun_km} kilometers from the sun. Lastly, it is the  #{@fun_fact}."
+    return "#{name} are the color #{color}. Their #{home} is #{distance_from_sun_ly} light years away from the sun. #{fun_fact}\n\n"
   end
 end
-
 
